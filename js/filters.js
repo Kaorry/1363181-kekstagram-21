c9a20b43da4d93852bf3509eb4547d187732f232
Svetlana <61006704+Kaorry@users.noreply.github.com>
2020-10-27 14:20:53 +0300

    Добавляет фильтры

---
+++ js/filters.js
@@ -0,0 +1,75 @@
+'use strict';
+
+(function () {
+  const RANDOM_PHOTO_AMOUNT = 10;
+  const DEBOUNCE_INTERVAL = 500;
+
+  const imageFilterList = document.querySelector(`.img-filters`);
+  const imageFilterForm = imageFilterList.querySelector(`.img-filters__form`);
+
+  const Filters = {
+    default: `filter-default`,
+    random: `filter-random`,
+    discussed: `filter-discussed`,
+  };
+
+  let currentFilter = imageFilterList.querySelector(`.img-filters__button--active`);
+  let changeHandler;
+
+  const onFilterChange = (event) => {
+    if (currentFilter !== event.target && event.target.classList.contains(`img-filters__button`)) {
+      currentFilter.classList.remove(`img-filters__button--active`);
+      currentFilter = event.target;
+      currentFilter.classList.add(`img-filters__button--active`);
+
+      triggerChange();
+    }
+  };
+
+  const show = () => {
+    imageFilterList.classList.remove(`img-filters--inactive`);
+    imageFilterForm.addEventListener(`click`, onFilterChange);
+
+    triggerChange();
+  };
+
+  const setChangeHandler = (handler) => {
+    changeHandler = handler;
+  };
+
+  const applyFilter = (photoList) => {
+    switch (currentFilter.id) {
+      case Filters.random: {
+        return photoList
+          .sort(() => {
+            return Math.random() - 0.5;
+          })
+          .slice(0, RANDOM_PHOTO_AMOUNT);
+      }
+
+      case Filters.discussed: {
+        return [...photoList].sort((left, right) => {
+          return right.comments.length - left.comments.length;
+        });
+      }
+
+      default:
+        return photoList;
+    }
+  };
+
+  const triggerChange = window.util.debounce(
+      () => {
+        if (changeHandler) {
+          changeHandler();
+        }
+      },
+      DEBOUNCE_INTERVAL
+  );
+
+  window.filters = {
+    show,
+    setChangeHandler,
+    applyFilter,
+  };
+})();
--- js/util.js
+++ js/util.js
@@ -3,13 +3,6 @@
 (function () {
   const KEY_ESCAPE = `Escape`;
 
-  const getRandom = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-
-  const getRandomItem = (array) => array[getRandom(0, array.length - 1)];
-
-  const generateArray = (length, generatorItem) => [...Array(length)]
-    .map(generatorItem);
-
   const createCancelKeyHandler = (fn) => {
     return function (event) {
       if (event.key === KEY_ESCAPE && fn(event.target)) {
@@ -20,11 +13,22 @@
 
   const between = (value, min, max) => Math.min(max, Math.max(min, value));
 
+  const debounce = (cb, interval) => {
+    let lastTimeout = null;
+
+    return (...parameters) => {
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(() => {
+        cb(...parameters);
+      }, interval);
+    };
+  };
+
   window.util = {
-    getRandom,
-    getRandomItem,
-    generateArray,
     createCancelKeyHandler,
     between,
+    debounce,
   };
 })();
